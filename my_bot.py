15bb0bf8096fd1383828c7d530ddc9a6ba2c5e40
Guy <guy.painsky@gmail.com>
2020-07-09 17:23:52 +0300

    test for ci

--- my_bot.py
+++ my_bot.py
@@ -34,7 +34,7 @@ def get_FB(update, context):
 
     FB = web.DataReader('fb', 'yahoo', start, end)
 
-    results = "FB state from " + start " to " end ":\n"
+    results = "FB state from " + start + " to " + end + ":\n" \
        "High = " + FB.High.to_string().split()[2] + "\n" \
        "Low = " + FB.Low.to_string().split()[2] + "\n" \
        "Open = " + FB.Open.to_string().split()[2] + "\n" \

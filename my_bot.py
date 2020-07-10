6c7ed6e3d58d4e7baa7c7d132d2feadb2a3cd486
Guy <guy.painsky@gmail.com>
2020-07-10 12:06:43 +0300

    111

--- my_bot.py
+++ my_bot.py
@@ -35,8 +35,7 @@ def get_FB(update, context):
 
     FB = web.DataReader('fb', 'yahoo', start, end)
 
-    #results = "FB state from " + start.strftime("%H:%M:%S") + " to " + end.strftime("%H:%M:%S") + ":\n" \
-    results = "FB state from :\n" \
+    results = "FB state from " + start.strftime("%H:%M:%S") + " to " + end.strftime("%H:%M:%S") + ":\n" \
        "High = " + FB.High.to_string().split()[2] + "\n" \
        "Low = " + FB.Low.to_string().split()[2] + "\n" \
        "Open = " + FB.Open.to_string().split()[2] + "\n" \
